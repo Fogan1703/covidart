ff9435e1b1871c17afaeea8bb091d9b70569d48b
Fogan1703 <blesk.bogdan@gmail.com>
2022-04-29 14:38:59 +0300

    Seen check

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,30 @@ import 'package:covidart/router.dart';
 import 'package:covidart/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const App());
+void main() async {
+  final prefs = await SharedPreferences.getInstance();
+
+  final seen = prefs.getBool('seen') ?? false;
+  if (seen == false) {
+    prefs.setBool('seen', true);
+  }
+
+  runApp(
+    App(
+      seen: seen,
+    ),
+  );
 }
 
 class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+  final bool seen;
+
+  const App({
+    Key? key,
+    required this.seen,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
